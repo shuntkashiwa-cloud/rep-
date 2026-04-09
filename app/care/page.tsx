0742e53b643450f7e4251f652ed03ffa3d77ad61
shuntkashiwa-cloud <shunt.kashiwa@gmail.com>
2026-04-09 22:40:39 +0900

    hukidashi

--- app/care/page.tsx
+++ app/care/page.tsx
@@ -1,20 +1,49 @@
 "use client";
 import TimeUI from "@/components/timeui";
-import { useEffect, useRef, useState } from "react";
+import { use, useEffect, useRef, useState } from "react";
 export default function Home() {
-  const [scheduler, setScheduler] = useState(new Array(12).fill(0).map((_, ind) => new Array(new Date(new Date().getFullYear(), (ind + 1) % 12, 0).getDate()).fill(false)));
-  const [firstDay, setFirstDay] = useState(new Date(new Date().getFullYear(), new Date().getMonth(), 1));
+  const shuryo = ["すべて17時", "すべて14時", "すべて一括で指定", "個別で指定"]
+  const nowmonth = new Date().getMonth()
+  const nowdate = new Date().getDate()
+
+  const [scheduler, setScheduler] = useState<(boolean | number)[][]>(new Array(12).fill(0).map((_, ind) => new Array(new Date(new Date().getFullYear(), (ind + 1) % 12, 0).getDate()).fill(false)));
+  const [firstDay, setFirstDay] = useState(new Date(new Date().getFullYear(), nowmonth, 1));
   const [shuryoType, setShuryoType] = useState(0);
   const [shuryoopend, setShuryoopend] = useState(false);
-  const [ikkatsuTime, setIkkatsuTime] = useState("17:00");
+  const [kboetunum, setKobetunum] = useState(31);
+  const [iskobeopend, setIskobeopend] = useState(false);
+  const [unseen, setunseen] = useState(false);
+  const unseto = useRef<ReturnType<typeof setTimeout>>(undefined)
 
   const fday = firstDay.getDay();
-  const shuryo = ["すべて17時", "すべて14時", "すべて一括で指定", "個別で指定"]
 
-  const setblue = (index: number) => {
-    return () => setScheduler(prev => {
+  const popupref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleclick = (e: MouseEvent) => {
+      const contain = popupref.current
+      if (contain && !contain.contains(e.target as Node)) {
+        setIskobeopend(false);
+        unseto.current = setTimeout(() => { setunseen(false) }, 300);
+      }
+    }
+    document.addEventListener("mousedown", handleclick);
+    return () => {
+      document.removeEventListener("mousedown", handleclick)
+    }
+  }, [])
+
+  const setalltime = (time: number) => {
+    setScheduler(prev => {
+      return prev.map((i) => (i.map((l) => (l ? time : false))))
+    })
+  }
+
+  const setblue = (index: number, numm?: number) => {
+    console.log(numm);
+    setScheduler(prev => {
       const newScheduler = structuredClone(prev);
-      newScheduler[firstDay.getMonth()][index] = !newScheduler[firstDay.getMonth()][index];
+      newScheduler[firstDay.getMonth()][index] = (newScheduler[firstDay.getMonth()][index] === false || numm ? numm || (17 * 60) : false);
       return newScheduler;
     })
   }
@@ -22,54 +51,69 @@ export default function Home() {
   return (
     <div className="**:transition **:duration-200 flex flex-col min-h-screen items-center gap-4 bg-zinc-50 text-black" onClick={() => { if (shuryoopend) setShuryoopend(false); }}>
       <h1 className="text-xl mt-2">音楽部残留予定</h1>
-      <div className="w-100 bg-white p-4 rounded shadow">
+      <div className="w-100 bg-white p-4 rounded shadow pb-40 mb-10">
         <p className="text-lg mb-2">活動日</p>
-        <div className="shadow flex flex-col gap-1 items-center size-fit bg-white outline outline-1 outline-gray-300 rounded ">
+        <div className="shadow flex flex-col gap-1 items-center size-fit bg-white outline-1 outline-gray-300 rounded ">
           <div className="flex items-center">
-            <button onClick={() => {
+            <button disabled={firstDay.getMonth() === nowmonth} onClick={() => {
               let newdate = new Date(firstDay);
               newdate.setMonth(firstDay.getMonth() - 1);
               setFirstDay(newdate)
-            }} className="px-2 py-1 rounded hover:bg-gray-300">{"<"}</button>
+            }} className={`px-2 py-1 rounded disabled:text-gray-400 enabled:hover:bg-gray-300`}>{"<"}</button>
             <h2 className="text-base mx-5">{firstDay.getFullYear()}年{firstDay.getMonth() + 1}月</h2>
-            <button onClick={() => {
+            <button disabled={firstDay.getMonth() === (nowmonth + 11) % 12} onClick={() => {
               let newdate = new Date(firstDay);
               newdate.setMonth(firstDay.getMonth() + 1);
               setFirstDay(newdate)
-            }} className="px-2 py-1 rounded hover:bg-gray-300">{">"}</button>
+            }} className={`px-2 py-1 rounded disabled:text-gray-400 enabled:hover:bg-gray-300`}>{">"}</button>
           </div>
           <div className="w-80 grid grid-cols-7 justify-around items-center text-sm">
-            {["日", "月", "火", "水", "木", "金", "土"].map((i, index) => (<p className={`text-center aspect-square ${index === 0 ? 'text-[oklch(0.563_0.1275_63.83)]' : ''}`} style={{ lineHeight: `${(75 / 7 / 4).toFixed(2)}rem` }} key={index}>{i}</p>))}
+            {["日", "月", "火", "水", "木", "金", "土"].map((i, index) => (<p className={`text-center aspect-square ${index === 0 ? 'text-ore' : ''}`} style={{ lineHeight: `${(75 / 7 / 4).toFixed(2)}rem` }} key={index}>{i}</p>))}
             {new Array(firstDay.getDay()).fill(0).map((_, index) => (<p className={`text-center aspect-square text-gray-400`} style={{ lineHeight: `${(80 / 7 / 4)}rem` }} key={index}>{index - firstDay.getDay() + 1 + scheduler[(firstDay.getMonth() + 11) % 12].length}</p>))}
-            {scheduler[firstDay.getMonth()].map((i, index) => (<p className={`text-center aspect-square rounded-full ${!i ? "hover:bg-gray-200" : ""} m-[0.12rem] ${(index + fday) % 7 === 0 ? ' text-[oklch(0.563_0.1275_63.83)]' : ''} ${i ? "bg-sky-700 text-white hover:bg-[oklch(0.544_0.146_242.358)]" : ""} `} style={{ lineHeight: `${(80 / 7 / 4) - 0.24}rem` }} key={index} onClick={setblue(index)}>{index + 1}</p>))}
+            {scheduler[firstDay.getMonth()].map((i, index) => (<p className={`${(nowmonth === firstDay.getMonth() && index + 1 < nowdate) ? "text-gray-400" : "cursor-pointer"} text-center aspect-square rounded-full ${(!i && (index + 1 >= nowdate || firstDay.getMonth() != nowmonth)) ? "hover:bg-gray-200" : ""} m-[0.12rem] ${(index + fday) % 7 === 0 ? ' text-ore' : ''} ${i ? "bg-sky-700 text-white hover:bg-[oklch(0.544_0.146_242.358)]" : ""} `} style={{ lineHeight: `${(80 / 7 / 4) - 0.24}rem` }} key={index} onClick={() => { if (index + 1 >= nowdate || firstDay.getMonth() != nowmonth) setblue(index) }}>{index + 1}</p>))}
           </div>
         </div>
         <p className="text-lg mb-2 mt-5">通常活動の終了時刻</p>
         <div className={`relative w-40 border border-gray-300 p-2 hover:border-sky-700 ${shuryoopend ? 'border-sky-700' : ''} cursor-pointer`}>
           <div onClick={() => setShuryoopend(prev => !prev)}>
             <p className="text-sm">{shuryo[shuryoType]}</p>
-            <svg className={`absolute right-2 top-1/2 -translate-y-1/2 w-4 h-4 transition !duration-400 ${shuryoopend ? "-rotate-180" : ""}`} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg className={`absolute right-2 top-1/2 -translate-y-1/2 w-4 h-4 transition duration-400! ${shuryoopend ? "-rotate-180" : ""}`} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M6 9L12 15L18 9" stroke="#333" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
             </svg>
           </div>
-          <div className={`absolute top-full left-0 w-full bg-gray-100 border border-gray-300 rounded mt-1 shadow-lg !duration-400 ${shuryoopend ? '' : 'opacity-0 scale-70 -translate-y-[15%] pointer-events-none'}`}>
+          <div className={`absolute z-20 top-full left-0 w-full bg-gray-100 border border-gray-300 rounded mt-1 shadow-lg duration-400! ${shuryoopend ? '' : 'opacity-0 scale-70 -translate-y-[15%] pointer-events-none'}`}>
             {shuryo.map((i, index) => (
               <p key={index} className={`text-sm p-2 hover:bg-gray-200 cursor-pointer ${index === shuryoType ? 'border border-sky-700' : ''}`} onClick={() => {
                 setShuryoType(index);
                 setShuryoopend(false);
+                if (index === 0) setalltime(17 * 60);
+                if (index === 1) setalltime(14 * 60);
               }}>{i}</p>
             ))}</div>
         </div>
         {shuryoType === 2 || shuryoType === 3 ? <p className="text-lg mb-2 mt-5">時刻を指定</p> : null}
         {shuryoType === 3 && (
-          <div className="w-80 grid grid-cols-7 justify-around items-center text-sm">
-            {["日", "月", "火", "水", "木", "金", "土"].map((i, index) => (<p className={`text-center aspect-square ${index === 0 ? 'text-[oklch(0.563_0.1275_63.83)]' : ''}`} style={{ lineHeight: `${(75 / 7 / 4).toFixed(2)}rem` }} key={index}>{i}</p>))}
-            {new Array(firstDay.getDay()).fill(0).map((_, index) => (<p className={`text-center aspect-square text-gray-400`} style={{ lineHeight: `${(80 / 7 / 4)}rem` }} key={index}>{index - firstDay.getDay() + 1 + scheduler[(firstDay.getMonth() + 11) % 12].length}</p>))}
-            {scheduler[firstDay.getMonth()].map((i, index) => (<p className={`text-center aspect-square rounded-full ${!i ? "hover:bg-gray-200" : ""} m-[0.12rem] ${(index + fday) % 7 === 0 ? ' text-[oklch(0.563_0.1275_63.83)]' : ''} ${i ? "bg-sky-700 text-white hover:bg-[oklch(0.544_0.146_242.358)]" : ""} `} style={{ lineHeight: `${(80 / 7 / 4) - 0.24}rem` }} key={index} onClick={setblue(index)}>{index + 1}</p>))}
+          <div className="relative">
+            <div className="w-80 grid grid-cols-7 justify-around items-center text-sm">
+              {["日", "月", "火", "水", "木", "金", "土"].map((i, index) => (<p className={`text-center aspect-square ${index === 0 ? 'text-ore' : ''}`} style={{ lineHeight: `${(75 / 7 / 4).toFixed(2)}rem` }} key={index}>{i}</p>))}
+              {new Array(firstDay.getDay()).fill(0).map((_, index) => (<p className={`text-center aspect-square text-gray-400`} style={{ lineHeight: `${(80 / 7 / 4)}rem` }} key={index}>{index - firstDay.getDay() + 1 + scheduler[(firstDay.getMonth() + 11) % 12].length}</p>))}
+              {scheduler[firstDay.getMonth()].map((i, index) => (<div key={index} className={`relative rounded-full m-[0.12rem]  ${i ? "cursor-pointer bg-sky-700 text-white hover:bg-[oklch(0.544_0.146_242.358)]" : ""}`} onClick={() => {
+                if (i) {
+                  setunseen(true);
+                  setKobetunum(index);
+                  setTimeout(() => { setIskobeopend(true) }, 10)
+                  clearTimeout(unseto.current);
+                }
+              }}><p className={`text-center aspect-square pt-2 rounded-full ${(index + fday) % 7 === 0 ? ' text-ore' : ''}`} key={index}>{index + 1}</p><p className="absolute top-6 text-white text-xs right-1/2 translate-x-1/2">{(typeof i == "number" && `${Math.floor(i / 60)}:${(i % 60).toString().padStart(2, "0")}`)}</p></div>))}
+            </div>
+            <div hidden={!unseen} ref={popupref} className={`${iskobeopend ? "" : "opacity-0 scale-70 translate-y-[15%]"} border border-gray-300 absolute -translate-x-1/2 bg-neutral-100 p-4 w-fit shadow-lg`} style={{ left: `${(fday + kboetunum + 1) % 7 * (80 * 4 / 7) - 80 * 2 / 7}px`, bottom: `${(Math.ceil((fday + scheduler[firstDay.getMonth()].length) / 7) - Math.ceil((fday + kboetunum + 1) / 7) + 1) * (80 * 4 / 7)+16}px` }}><TimeUI key={kboetunum} type="w" settime={(i) => { setblue(kboetunum, i) }} />
+              <svg viewBox="0 0 28 28" className="w-7 h-7 absolute -bottom-7 left-1/2 -translate-x-1/2">
+                <path d="M0 0L14 16 L28 0Z " className="fill-neutral-100 stroke-1 stroke-gray-300"></path>
+                <path d="M1 0L27 0" className="stroke-neutral-100"></path></svg>
+            </div>
           </div>
         )}
-        {shuryoType === 2 && (<input type="time" className="border border-gray-300 outline-none p-2 focus:border-sky-700 text-sm" step="300" value={ikkatsuTime} onChange={(e) => setIkkatsuTime(e.target.value)} ></input>)}
-        <TimeUI/>
+        {shuryoType === 2 && <TimeUI settime={setalltime} />}
       </div>
     </div>
   )
--- components/timeui.tsx
+++ components/timeui.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useRef, useState } from "react";
-export default function TimeUI() {
+type Props={settime:(time:number)=>void; type?:string};
+export default function TimeUI({settime,type}:Props) {
 
   const [hour, setHour] = useState([17, 0]);
   const [dur, setDur] = useState(200);
@@ -15,13 +16,13 @@ export default function TimeUI() {
   const opa = ["100", "70", "50", "0"];
   const position = ["-translate-y-[52px]", "-translate-y-[47px]", "-translate-y-[32px]", "-translate-y-[12px]", "translate-y-[8px]", "translate-y-[23px]", "translate-y-[28px]"];
   const scale = ["100", "80", "50", "0"];
+  const hourref=useRef([17,0])
 
   //キー入力での時間変更のための関数。入力値が不正な場合は最後に有効だった値を使用する。
   const lasthour = useRef([17, 0]);
   const count = useRef([0, 0]);
   const [focused, setFocused] = useState([false, false]);
   const pRef = useRef<(HTMLParagraphElement|null)[]>([null, null]);
-
 
   const setsethour = (i: number, val: number) => {
     let valval=0;
@@ -56,6 +57,8 @@ export default function TimeUI() {
       const timeout = Math.max(Date.now() - last.current, lastdur.current);
       if (timeout > 190) {
         reserved.current = false;
+        console.log(60*hour[0]+hour[1])
+        settime(60*hourref.current[0]+hourref.current[1]);
         return;
       }
       iflastscro = false;
@@ -104,9 +107,13 @@ export default function TimeUI() {
     }
   }, []);
 
+  useEffect(()=>{
+    hourref.current=hour;
+  },[hour])
 
   return (
     <div>
+      <div className="bg-white flex px-1 mb-2 shadow w-fit">{[17,14].map((i)=>(<button key={i} className="px-2 py-1.5 relative before:transition-transform hover:before:duration-400 hover:before:ease-in-out before:ease-out before:duration-200 before:absolute before:bottom-0 before:left-0 before:h-0.5 before:w-full before:origin-right hover:before:origin-left before:scale-x-0 before:bg-ore before:content-[''] hover:before:scale-x-100 " onClick={()=>{settime(60*i);setHour([i,0])}}>{i}:00</button>))}</div>
       <p ref={(el) => {pRef.current[0] = el}} tabIndex={0} className="!duration-0 text-center border border-gray-300 w-6 focus:border-sky-700 outline-none" onKeyDown={(e) => {
         const allowedKeys = [
           'Backspace',
@@ -132,14 +139,14 @@ export default function TimeUI() {
             }
           }
         }
-      }} onFocus={() => { setFocused([true, false]); count.current[0] = 0; }} onBlur={() => setFocused((prev) => { const buf = [...prev]; buf[0] = false; return buf; })}>{focused[0] ? <mark className="p-px bg-[oklch(0.54_0.17_262.17)] text-white">{(hour[0]).toString().padStart(2, '0')}</mark> :(hour[0]+24%24).toString().padStart(2, '0')}</p>
-      <div className="relative bg-gray-100 border border-gray-300 rounded mt-1 shadow-lg h-24 w-[161px]">
+      }} onFocus={() => { setFocused([true, false]); count.current[0] = 0; }} onBlur={() => {settime(hour[0]*60+hour[1]);setFocused((prev) => { const buf = [...prev]; buf[0] = false; return buf; })}}>{focused[0] ? <mark className="p-px bg-[oklch(0.54_0.17_262.17)] text-white">{(hour[0]).toString().padStart(2, '0')}</mark> :(hour[0]+24%24).toString().padStart(2, '0')}</p>
+      <div className={`relative ${type? "bg-white":"bg-gray-100"} border border-gray-300 rounded mt-1 shadow-lg h-24 w-[161px]`}>
         {["32", "79"].map((i, inde) => (
           <div key={inde} className={`absolute h-24 w-12 -top-[1px] outline-1 -outline-offset-1 outline-sky-700 z-5 ${ikkatsuhover[inde] ? 'opacity-100' : 'opacity-0'}`} style={{ left: `${i}px` }}></div>
         ))}
         <div ref={scroh} className="absolute z-10 h-24 w-20 top-0 left-0"></div>
         <div ref={scrom} className="absolute z-20 h-24 w-20 top-0 left-20"></div>
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[50%] w-[161px] rounded h-5 bg-gray-300"></div>
+        <div className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[50%] w-[161px] rounded h-5 ${type? "bg-gray-200":"bg-gray-300"}`}></div>
         {[24, 60].map((i, inde) => (
           new Array(i).fill(0).map((_, index) => (<p key={index} className={`w-8 text-center z-1 whitespace-pre tabular-nums absolute top-1/2  -translate-x-1/2 ${position[Math.max(0, Math.min(6, (index - hour[inde] + i * 3 / 2) % i - i / 2 + 3))]}`} style={
             {
