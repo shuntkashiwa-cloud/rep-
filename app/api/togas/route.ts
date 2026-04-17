085478f8f8506a5a20fceac95d3d54e03c009305
shuntkashiwa-cloud <shunt.kashiwa@gmail.com>
2026-04-17 23:48:43 +0900

    stringerror

--- app/api/togas/route.ts
+++ app/api/togas/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse} from "next/server";
 export async function POST(request: Request) {
     const data = await request.json();
-    const mesage=await fetch(process.env.GAS_URL!, { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(data) });
+    const mesage=await fetch(process.env.GAS_URL!, { method: "POST", headers: { "Content-Type": "application/json" }, body: data });
     const d=await mesage.json();
-    return NextResponse.json({ message: JSON.stringify(d) });
+    return NextResponse.json({ message: d });
 }
