75582190492b5562e7f8ef0515d689f00b4a7d06
shuntkashiwa-cloud <shunt.kashiwa@gmail.com>
2026-04-22 23:05:35 +0900

    updisNotTested

--- userint/app/api/senddis/route.ts
+++ userint/app/api/senddis/route.ts
@@ -16,35 +16,45 @@ export async function POST(req: Request) {
 
   for (let i = 1; i < 8; i++) {
     if (new Date(now.getFullYear(), 2, 0).getDate() == 28 && i + current + j == 59) j = 1;
-    if (iit[(i + current + j)%366].time) {
+    if (iit[(i + current + j) % 366].time) {
       buf.push(i);
-      bufsupa.push({ "id": i + current + j, "time": iit[(i + current + j)%366].time });
+      bufsupa.push({ "id": i + current + j, "time": iit[(i + current + j) % 366].time });
     }
   }
-  const res = await fetch(`https://discord.com/api/v10/channels/${process.env.DISCORD_CHANNEL_ID}/messages`, {
-    method: "POST",
-    headers: header,
-    body: JSON.stringify({
-      content: `今週の出欠です。残留したい曜日に対応する番号でリアクションしてください。\n${buf.map((day) => `${day}:${["月", "火", "水", "木", "金", "土", "日"][day - 1]}`).join("　")}`
-    })
-  });
-  const sleep = (value: number) => { return new Promise((resolve) => setTimeout(resolve, value)) };
 
-  const resres = await res.json();
-  for (const day of buf) {
-    await fetch(`https://discord.com/api/v10/channels/${process.env.DISCORD_CHANNEL_ID}/messages/${resres.id}/reactions/${emos[day - 1]}/@me`, {
-      method: "PUT",
-      headers: header
+  let resres;
+  let d={"result": "nonedata"};
+
+  if (buf.length !== 0) {
+
+    const res = await fetch(`https://discord.com/api/v10/channels/${process.env.DISCORD_CHANNEL_ID}/messages`, {
+      method: "POST",
+      headers: header,
+      body: JSON.stringify({
+        content: `今週の出欠です。残留したい曜日に対応する番号でリアクションしてください。\n${buf.map((day) => `${day}:${["月", "火", "水", "木", "金", "土", "日"][day - 1]}`).join("　")}`
+      })
     });
-    await sleep(100);
-  }
-  const oldid=await supabase.from("messageid").select().eq("id", 0).single()
-  if (oldid.data){
-    await supabase.from("messageid").update({ id: 0, messageid: oldid.data.messageid,date:31*now.getMonth()+now.getDate()}).eq("id", 0);
-  }
-  await supabase.from("messageid").update({ id: 1, messageid: resres.id ,date:31*now.getMonth()+now.getDate()}).eq("id", 1);
+    const sleep = (value: number) => { return new Promise((resolve) => setTimeout(resolve, value)) };
 
-  const mesage = await fetch(process.env.GAS_URL!, { method: "POST", headers: header, body: JSON.stringify(bufsupa) });
-  const d = await mesage.json();
-  return NextResponse.json({ message: d});
+    resres = await res.json();
+    for (const day of buf) {
+      await fetch(`https://discord.com/api/v10/channels/${process.env.DISCORD_CHANNEL_ID}/messages/${resres.id}/reactions/${emos[day - 1]}/@me`, {
+        method: "PUT",
+        headers: header
+      });
+      await sleep(100);
+    }
+
+    const mesage = await fetch(process.env.GAS_URL!, { method: "POST", headers: header, body: JSON.stringify(bufsupa) });
+    d = await mesage.json();
+  }
+
+  const oldid = await supabase.from("messageid").select().eq("id", 1).single()
+  if (oldid.data) {
+    await supabase.from("messageid").update({ id: 0, messageid: oldid.data.messageid, date: oldid.data.date }).eq("id", 0);
+  }
+  await supabase.from("messageid").update({ id: 1, messageid: (buf.length == 0 ? null : resres.id), date: 31 * now.getMonth() + now.getDate() }).eq("id", 1);
+
+
+  return NextResponse.json({ message: d });
 }
--- userint/app/api/updatedis/route.ts
+++ userint/app/api/updatedis/route.ts
@@ -1,15 +1,17 @@
 import { NextResponse } from 'next/server';
 import { supabase } from "../../../lib/supabaseClient";
 export async function POST(req: Request) {
-
+  const emos = ["\u0031", "\u0032", "\u0033", "\u0034", "\u0035", "\u0036", "\u0037"].map((num) => encodeURIComponent(num + "\ufe0f\u20E3"));
 
   const subday = await req.json()
   const all = subday.disbuf
   const hue = subday.hue
   const heri = subday.heri
   let nowbuf = new Date();
-  nowbuf.setDate(nowbuf.getDate() -nowbuf .getDay());
-  const now=31*nowbuf.getMonth()+nowbuf.getDate();
+  nowbuf.setDate(nowbuf.getDate() - nowbuf.getDay());
+  const now = 31 * nowbuf.getMonth() + nowbuf.getDate();
+  nowbuf.setDate(nowbuf.getDate() - 7);
+  const last = 31 * nowbuf.getMonth() + nowbuf.getDate();
   const { data, error } = await supabase.from("messageid").select().order("id", { ascending: true });
   const header = {
     "Content-Type": "application/json",
@@ -17,21 +19,46 @@ export async function POST(req: Request) {
   }
 
   if (!data) return NextResponse.json({ message: "No data found" }, { status: 404 });
-  if (data[1].date == now) {
+  const ind = data.findIndex((item) => item.date == now);
+  if (ind !== -1) {
+    const res = await fetch(`https://discord.com/api/v10/channels/${process.env.DISCORD_CHANNEL_ID}/messages${data[ind].messageid ? `/${data[ind].messageid}` : ''}`, {
+      method: `${data[ind].messageid ? "PATCH" : "POST"}`,
+      headers: header,
+      body: JSON.stringify({
+        content: `今週の出欠です。残留したい曜日に対応する番号でリアクションしてください。\n${all.map((day: number) => day !== 0 ? `${day}:${["月", "火", "水", "木", "金", "土", "日"][day - 1]}` : "").join("　")}`
+      })
+    });
+    const resres = await res.json();
     for (const day of hue) {
-      await fetch(`https://discord.com/api/v10/channels/${process.env.DISCORD_CHANNEL_ID}/messages/${data[1].messageid}/reactions/${encodeURIComponent(emos[day] + "\ufe0f\u20E3")}/@me`, {
-        method: "PUT",
-        headers: header
-      });
+      if (day !== 0) await fetch(`https://discord.com/api/v10/channels/${process.env.DISCORD_CHANNEL_ID}/messages/${resres.id}/reactions/${encodeURIComponent(emos[day - 1])}/@me`, { method: "PUT", headers: header });
+    }
+    for (const day of heri) {
+      if (day !== 0) await fetch(`https://discord.com/api/v10/channels/${process.env.DISCORD_CHANNEL_ID}/messages/${resres.id}/reactions/${encodeURIComponent(emos[day - 1])}`, { method: "DELETE", headers: header });
     }
   }
+  const ind2 = data.findIndex((item) => item.date == last);
+  if(ind2!==-1 && all[0]==0){
+    const mescont = await fetch(`https://discord.com/api/v10/channels/${process.env.DISCORD_CHANNEL_ID}/messages/${data[ind2].messageid}`, {
+      method: "GET",
+      headers: header
+    }).then((res) => res.json()).then((res) => res.content);
+    let bun,type;
+    if(mescont.slice(-3)==="7:日" && heri[0]==0){
+      bun=mescont.slice(0, -4);
+      type = "DELETE";
+    }
+    if(mescont.slice(-3)!=="7:日" && hue[0]==0){
+      bun=mescont + "　7:日";
+      type = "PUT"; 
+    }
+    const res=await fetch(`https://discord.com/api/v10/channels/${process.env.DISCORD_CHANNEL_ID}/messages${data[ind2].messageid ? "/" + data[ind2].messageid : ''}`, {
+      method: (data[ind2].messageid ?"PATCH" : "POST"),
+      headers: header,
+      body: JSON.stringify({content: bun})
+    }).then((res) => res.json()).then((res) => res.id);
 
-  const res = await fetch(`https://discord.com/api/v10/channels/${process.env.DISCORD_CHANNEL_ID}/messages`, {
-    method: "POST",
-    headers: header,
-    body: JSON.stringify({
-      content: `今週の出欠です。残留したい曜日に対応する番号でリアクションしてください。\n${all.map((day: number) => day !== 0 ? `${day}:${["月", "火", "水", "木", "金", "土", "日"][day - 1]}` : "").join("　")}`
-    })
-  });
+    await fetch(`https://discord.com/api/v10/channels/${process.env.DISCORD_CHANNEL_ID}/messages/${res}/reactions/${encodeURIComponent(emos[6])}/@me`, { method: "PUT", headers: header });
+  }
+
   return NextResponse.json({ message: subday });
 }
